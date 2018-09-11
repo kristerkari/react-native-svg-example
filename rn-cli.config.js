6447b3ebb06661c45e93aad6ff8001dabfac8f35
krister <krister@sent.at>
2018-09-11 21:12:49 +0300

    Update rn-cli.config and svg transformer

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-  transformModulePath: require.resolve("react-native-svg-transformer"),
-  resolver: {
-    sourceExts: ["svg"]
-  }
-};
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
