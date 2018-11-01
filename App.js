160097630b68f22afe6787f1fecb6adae3259f85
krister <krister@sent.at>
2018-11-01 21:29:12 +0200

    Add more logos

--- App.js
+++ App.js
@@ -8,11 +8,16 @@
 
 import React, { Component } from "react";
 import { SafeAreaView, ScrollView } from "react-native";
+import Autocode from "./logos/autocode.svg";
 import Bower from "./logos/bower.svg";
 import Composer from "./logos/composer.svg";
 import Drupal from "./logos/drupal.svg";
+import Egghead from "./logos/egghead.svg";
 import Ember from "./logos/ember.svg";
+import Hostgator from "./logos/hostgator.svg";
+import Pug from "./logos/pug.svg";
 import Snyk from "./logos/snyk.svg";
+import Stylelint from "./logos/stylelint.svg";
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -28,6 +33,11 @@ export default class App extends Component<Props> {
           <Drupal width={256} height={295} style={styles.logo} />
           <Composer width={256} height={339} style={styles.logo} />
           <Ember width={256} height={245} style={styles.logo} />
+          <Autocode width={256} height={241} style={styles.logo} />
+          <Egghead width={256} height={263} style={styles.logo} />
+          <Hostgator width={256} height={348} style={styles.logo} />
+          <Pug width={256} height={256} style={styles.logo} />
+          <Stylelint width={256} height={245} style={styles.logo} />
         </ScrollView>
       </SafeAreaView>
     );
