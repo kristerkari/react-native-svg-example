bd737665fadcc79c82ecb9772b151714245403ff
Krister Kari <kristerkari@users.noreply.github.com>
2023-07-05 21:44:18 +0300

    Update React Native to 0.72 (#55)

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ["@babel/plugin-proposal-private-property-in-object", {
-      "loose": true
-    }],
-    ["@babel/plugin-proposal-class-properties", {
-      "loose": true
-    }],
-    ["@babel/plugin-proposal-private-methods", {
-      "loose": true
-    }]
-  ]
-}
+    [
+      '@babel/plugin-proposal-private-property-in-object',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-private-methods',
+      {
+        loose: true,
+      },
+    ],
+  ],
+};
