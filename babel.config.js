30e4c4e4ad38513a5e29256f02b2b05f401ecb72
Krister Kari <krister@sent.at>
2023-12-11 19:03:10 +0200

    Update react-native-svg-transformer to 1.2.0

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      '@babel/plugin-proposal-private-property-in-object',
-      {
-        loose: true,
-      },
-    ],
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
-    [
-      '@babel/plugin-proposal-private-methods',
-      {
-        loose: true,
-      },
-    ],
-  ],
 };
